45380aac820cfde023f1e9c117a2d639c91a5247
Luke Bubar <lukerb52@gmx.com>
2020-06-12 11:30:51 -0400

    On branch master  Changes to be committed: 	modified:   podcon.sh

--- podcon.sh
+++ podcon.sh
@@ -2,14 +2,32 @@
 
 # A script that converts audio/video files into formats compatible with
 
-file=$(ls -A | grep -i --include=\*.{ogg,OGG,opus,OPUS} '' | sed -n 1p ) #> ~/.cache/podcon
-
-#ffmpeg -i input.wav -vn -ar 44100 -ac 2 -b:a 192k output.mp3
+file=$(ls -A | grep -i --include=\*.{ogg,OGG,opus,OPUS,m4a,M4A} '' | sed -n 1p ) #> ~/.cache/podcon
 
 echo $file
+
+#bs=$(echo $file | sed 's/ /\\ /g')
+
+#echo $bs
+
+#tk=$(echo $file | sed 's/ /_/g')
+
+#echo $tk
+
+#cp $bs $tk
+
+if [ -d "mp3" ]; then
+    echo "mp3 is a directory."
+else
+	mkdir mp3
+fi
 
 base="${file%.*}"
 
 echo $base
 
-ffmpeg -hide_banner -i $file -ac 2 $base.mp3
+echo "$file"
+
+ffmpeg -hide_banner -i "$file" -ac 2 mp3/"$base".mp3
+
+#ffmpeg -i input.wav -vn -ar 44100 -ac 2 -b:a 192k output.mp3
